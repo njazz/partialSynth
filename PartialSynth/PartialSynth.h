72425336b46757e1db2d472d66f1155a38c21d08
Alex Nadzharov <anadjarov@gmail.com>
2018-03-09 01:57:11 +0300

    iOS framework target added

--- PartialSynth/PartialSynth.h
+++ PartialSynth/PartialSynth.h
@@ -16,4 +16,8 @@ FOUNDATION_EXPORT const unsigned char PartialSynthVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PartialSynth/PublicHeader.h>
 
-
+#include "BasePartial.hpp"
+#include "SinePartial.hpp"
+#include "ResPartial.hpp"
+#include "SineSynth.hpp"
+#include "ResSynth.hpp"
